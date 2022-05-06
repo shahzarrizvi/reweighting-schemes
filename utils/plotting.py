bcd6017f2b40a5dcdd12a1c74b2d9a02c75ba502
Shahzar <shahzar@berkeley.edu>
2022-05-06 11:31:39 -0700

    new reweighting schemes set-up

--- utils/plotting.py
+++ utils/plotting.py
@@ -12,7 +12,7 @@ rc('ytick', labelsize=15)
 rc('legend', fontsize=15)
 
 # Plotting functions
-def get_preds(model_lrs, xs=np.linspace(4, 16, 1000)):
+def get_preds(model_lrs, xs=np.linspace(-6, 6, 1000)):
     # Takes in model_lrs, a list of model likelihood ratios and xs, a list of 
     # values on which to compute the likelihood ratios. Returns a 2D array. The 
     # nth row is the likelihood ratio predictions from the nth model in 
@@ -24,7 +24,7 @@ def avg_lr(preds):
     # Returns the average likelihood ratio prediction and its error.
     return preds.mean(axis=0), preds.std(axis=0)
 
-def avg_lrr(preds, xs=np.linspace(4, 16, 1000)):
+def avg_lrr(lr, preds, xs=np.linspace(-6, 6, 1000)):
     # Takes in a 2D array of multiple models' likelihood ratio predictions. 
     # Returns the average ratio of predicted likelihood to true likelihood and 
     # its error.
@@ -32,13 +32,16 @@ def avg_lrr(preds, xs=np.linspace(4, 16, 1000)):
     return lrr_preds.mean(axis=0), lrr_preds.std(axis=0)
     
 def lr_plot(ensembles,
+            lr,
+            bkgd, 
+            sgnl,
             title=None,
             filename=None,
             cs = ['brown', 'green', 'red', 'blue'],
             lss = ['-', '--', '-.', ':'],
-            xs=np.linspace(0, 20, 1000)):
+            xs=np.linspace(-6, 6, 1000)):
+            #xs=np.linspace(0, 20, 1000)):
             #xs=np.linspace(4, 16, 1000)):
-            #xs=np.linspace(-6, 6, 1000)):
     # Takes in a list of pairs (lr_avg, lr_err). Plots them against the true 
     # likelihood.
     fig, ax_1 = plt.subplots(figsize = (10, 6))
@@ -59,10 +62,12 @@ def lr_plot(ensembles,
     # Plot background and signal
     ax_2 = ax_1.twinx()
     #bins = np.linspace(4, 16, 100)
-    bins = np.linspace(0, 20, 100)
-    #bins = np.linspace(-6, 6, 100)
-    plt.hist(sgnl, alpha=0.1, bins=bins)
-    plt.hist(bkgd, alpha=0.1, bins=bins)
+    #bins = np.linspace(0, 20, 100)
+    bins = np.linspace(-6, 6, 100)
+    X_bkgd = bkgd.rvs(size = 10**6)
+    X_sgnl = sgnl.rvs(size = 10**6)
+    plt.hist(X_sgnl, alpha=0.1, bins=bins)
+    plt.hist(X_bkgd, alpha=0.1, bins=bins)
     ax_2.minorticks_on()
     ax_2.tick_params(direction='in', which='both',length=5)
     plt.ylabel('Count')
@@ -70,21 +75,23 @@ def lr_plot(ensembles,
     plt.xlim(xs[0], xs[-1])
     plt.xlabel(r'$x$')
     #plt.title(r"$\mu_{\rm{sgnl}}="+str(10.1)+r", \mu_{\rm{bkgd}}="+str(9.9)+r"$",loc="right",fontsize=20)
-    plt.title(r"$r_{\rm{sgnl}}="+str(r)+r", r_{\rm{bkgd}}="+str(r+1)+r"$",loc="right",fontsize=20)
-    #plt.title(r"$\mu_{\rm{sgnl}}="+str(mu)+r", \mu_{\rm{bkgd}}="+str(-mu)+r"$",loc="right",fontsize=20)
+    #plt.title(r"$r_{\rm{sgnl}}="+str(r)+r", r_{\rm{bkgd}}="+str(r+1)+r"$",loc="right",fontsize=20)
+    plt.title(r"$\mu_{\rm{sgnl}}="+str(0.1)+r", \mu_{\rm{bkgd}}="+str(-0.1)+r"$",loc="right",fontsize=20)
     if title != None:
         plt.title(title, loc="left", fontsize=20)
     if filename != None:
         plt.savefig(filename, dpi=1200, bbox_inches='tight')
 
 def lrr_plot(ensembles,
+             lr,
+             bkgd, sgnl,
              title=None,
              filename=None,
              cs = ['brown', 'green', 'red', 'blue'],
              lss = [':', '--', '-.', ':'],
-             xs=np.linspace(0, 20, 1000)):
+             xs=np.linspace(-6, 6, 1000)):
+             #xs=np.linspace(0, 20, 1000)):
              #xs=np.linspace(4, 16, 1000)):
-             #xs=np.linspace(-6, 6, 1000)):
     # Takes in a list of pairs (lrr_avg, lrr_err). Plots them.
     fig, ax_1 = plt.subplots(figsize = (10, 6))
     
@@ -104,10 +111,12 @@ def lrr_plot(ensembles,
     # Plot background and signal
     ax_2 = ax_1.twinx()
     #bins = np.linspace(4, 16, 100)
-    bins = np.linspace(0, 20, 100)
-    #bins = np.linspace(-6, 6, 100)
-    plt.hist(sgnl, alpha=0.1, bins=bins)
-    plt.hist(bkgd, alpha=0.1, bins=bins)
+    #bins = np.linspace(0, 20, 100)
+    bins = np.linspace(-6, 6, 100)
+    X_bkgd = bkgd.rvs(size = 10**6)
+    X_sgnl = sgnl.rvs(size = 10**6)
+    plt.hist(X_sgnl, alpha=0.1, bins=bins)
+    plt.hist(X_bkgd, alpha=0.1, bins=bins)
     ax_2.minorticks_on()
     ax_2.tick_params(direction='in', which='both',length=5)
     plt.ylabel('Count')
@@ -115,8 +124,8 @@ def lrr_plot(ensembles,
     plt.xlim(xs[0], xs[-1])
     plt.xlabel(r'$x$')
     #plt.title(r"$\mu_{\rm{sgnl}}="+str(10.1)+r", \mu_{\rm{bkgd}}="+str(9.9)+r"$",loc="right",fontsize=20)
-    plt.title(r"$r_{\rm{sgnl}}="+str(r)+r", r_{\rm{bkgd}}="+str(r+1)+r"$",loc="right",fontsize=20)
-    #plt.title(r"$\mu_{\rm{sgnl}}="+str(mu)+r", \mu_{\rm{bkgd}}="+str(-mu)+r"$",loc="right",fontsize=20)
+    #plt.title(r"$r_{\rm{sgnl}}="+str(r)+r", r_{\rm{bkgd}}="+str(r+1)+r"$",loc="right",fontsize=20)
+    plt.title(r"$\mu_{\rm{sgnl}}="+str(0.1)+r", \mu_{\rm{bkgd}}="+str(-0.1)+r"$",loc="right",fontsize=20)
     if title != None:
         plt.title(title, loc="left", fontsize=20)
     if filename != None:
