ef689634842a8bb630347368446081331268b9a6
Shahzar <srizvi@lbl.gov>
2022-07-08 20:27:02 -0700

    finished running vectored loss landscapes. did some iterative minimization of sqr to see why there's a discrepancy between empirical and numerical

--- run_loss_landscapes.py
+++ run_loss_landscapes.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 
 np.random.seed(666)
 eps = 1e-7
-print(eps)
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1" # pick a number < 4 on ML4HEP; < 3 on Voltan 
 physical_devices = tf.config.list_physical_devices('GPU') 
@@ -25,54 +24,37 @@ sgnl = stats.norm(+0.1, 1)
 def nbce(a, b):
     g = lambda x: -(sgnl.pdf(x) * np.log(sig(a*x + b) + eps) + \
                     bkgd.pdf(x) * np.log(1 - sig(a*x + b) + eps) )
-    return integrate.quad(g, -np.inf, np.inf)[0]
+    return integrate.quad_vec(g, -np.inf, np.inf)[0]
 
 def nmse(a, b):
     g = lambda x: -(sgnl.pdf(x) * -(1 - sig(a*x + b))**2 + \
                     bkgd.pdf(x) * -(sig(a*x + b)**2) )
-    return integrate.quad(g, -np.inf, np.inf)[0]
+    return integrate.quad_vec(g, -np.inf, np.inf)[0]
 
 def nmlc(a, b):
     g = lambda x: -(sgnl.pdf(x) * np.log(relu(a*x + b) + eps) + \
                     bkgd.pdf(x) * (1 - relu(a*x + b)) )
-    if a == 0:
-        return integrate.quad(g, -np.inf, np.inf)[0]
-    return integrate.quad(g, -b/a, np.inf)[0]
+    return integrate.quad_vec(g, -np.inf, np.inf)[0]
 
 def nsqr(a, b):
     g = lambda x: -(sgnl.pdf(x) * -1 / (relu(a*x + b) + eps)**0.5 + \
                     bkgd.pdf(x) * -relu(a*x + b)**0.5 )
-    if a == 0:
-        return integrate.quad(g, -np.inf, np.inf)[0]
-    return integrate.quad(g, -b/a, np.inf)[0]
+    return integrate.quad_vec(g, -np.inf, np.inf)[0]
 
-g = np.meshgrid(np.linspace(-1.5, 1.5, 301), np.linspace(-1.5, 1.5, 301))
-g = np.round(np.append(g[0].reshape(-1, 1), g[1].reshape(-1, 1), axis = 1), 3)
-xs = g[:, 0]
-ys = g[:, 1]
+aa, bb = np.meshgrid(np.linspace(-1.5, 1.5, 301), np.linspace(-1.5, 1.5, 301))
 
-#nbce_zs = np.zeros(g.shape[0])
-#nmse_zs = np.zeros(g.shape[0])
-nmlc_zs = np.zeros(g.shape[0])
-nsqr_zs = np.zeros(g.shape[0])
+nbce_zs = nbce(aa, bb)
+nmse_zs = nmse(aa, bb)
+nmlc_zs = nmlc(aa, bb)
+nsqr_zs = nsqr(aa, bb)
 
-for i in range(g.shape[0]):
-    #nbce_zs[i] = nbce(g[i][0], g[i][1])
-    #nmse_zs[i] = nmse(g[i][0], g[i][1])
-    nmlc_zs[i] = nmlc(g[i][0], g[i][1])
-    nsqr_zs[i] = nsqr(g[i][0], g[i][1])
-    if i % 100 == 0:
-        #print(i, ':', nbce_zs[-1], nmse_zs[-1], nmlc_zs[-1], nsqr_zs[-1]) 
-        print(i, g[i], '\t:', nmlc_zs[i], '\t', nsqr_zs[i]) 
-    i += 1
-
-#np.save('anims/nbce_zs_2', nbce_zs)
-#np.save('anims/nmse_zs_2', nmse_zs)
+np.save('anims/nbce_zs_2', nbce_zs)
+np.save('anims/nmse_zs_2', nmse_zs)
 np.save('anims/nmlc_zs_2', nmlc_zs)
 np.save('anims/nsqr_zs_2', nsqr_zs)
 
 print('Numerical Estimation')
-#print(g[nbce_zs == min(nbce_zs)])
-#print(g[nmse_zs == min(nmse_zs)])
+print(g[nbce_zs == min(nbce_zs)])
+print(g[nmse_zs == min(nmse_zs)])
 print(g[nmlc_zs == min(nmlc_zs)])
 print(g[nsqr_zs == min(nsqr_zs)])
